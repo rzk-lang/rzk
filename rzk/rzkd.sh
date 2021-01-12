0c65659d03d24c1e85142b3025400c6ef6aab300
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2021-01-12 17:51:00 +0300

    Add file-watch typecheck script

---
+++ rzk/rzkd.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+FILE=$1
+
+old="first time"
+
+while true; do
+  new=$(stat -f %m "$FILE")
+  if [ "$old" != "$new" ]; then
+    old=$new
+    clear
+    stack build rzk && stack exec -- rzk typecheck "$FILE"
+    sleep 1
+  fi
+done
