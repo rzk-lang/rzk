ead0ba49734271142eb63fc71eb0e6125878d554
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2021-02-27 20:29:29 +0300

    Add doctests test suite

---
+++ rzk/test/doctests.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import           Build_doctests (flags, module_sources, pkgs)
+import           Data.Foldable  (traverse_)
+import           Test.DocTest
+
+main :: IO ()
+main = do
+  traverse_ putStrLn args
+  doctest args
+  where
+    args = flags ++ pkgs ++ module_sources
