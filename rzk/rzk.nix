e0045ba59dc8661f1e0ab60f06937488f1276d0f
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2023-09-19 14:11:21 +0200

    Update rzk.nix

--- rzk/rzk.nix
+++ rzk/rzk.nix
@@ -7,18 +7,23 @@ mkDerivation {
   version = "0.5.4";
   src = ./.;
   isLibrary = true;
-  isExecutable = false;
+  isExecutable = true;
   libraryHaskellDepends = [
-    aeson array base bifunctors bytestring mtl optparse-generic
+    aeson array base bifunctors bytestring Glob mtl optparse-generic
     template-haskell text
   ];
   libraryToolDepends = [ alex happy hpack ];
+  executableHaskellDepends = [
+    aeson array base bifunctors bytestring Glob mtl optparse-generic
+    template-haskell text
+  ];
+  executableToolDepends = [ alex happy ];
   testHaskellDepends = [
     aeson array base bifunctors bytestring doctest Glob mtl
     optparse-generic QuickCheck template-haskell text
   ];
   testToolDepends = [ alex happy ];
-  /* prePatch = "hpack"; */
+  prePatch = "hpack";
   homepage = "https://github.com/rzk-lang/rzk#readme";
   description = "An experimental proof assistant for synthetic ∞-categories";
   license = lib.licenses.bsd3;
