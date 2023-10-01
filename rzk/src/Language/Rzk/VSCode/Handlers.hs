3291cd024fd1f9597998f719cd447a7cccbbf11c
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-10-01 04:02:46 +0300

    fix: import 'when'

--- rzk/src/Language/Rzk/VSCode/Handlers.hs
+++ rzk/src/Language/Rzk/VSCode/Handlers.hs
@@ -8,7 +8,7 @@ module Language.Rzk.VSCode.Handlers where
 
 import           Control.Exception             (SomeException, evaluate, try)
 import           Control.Lens
-import           Control.Monad                 (forM_)
+import           Control.Monad                 (forM_, when)
 import           Control.Monad.IO.Class        (MonadIO (liftIO))
 import           Data.Default.Class
 import           Data.List                     (sort, (\\))
