afd41dd2c4485db65868e839f48421ba9e79b4b8
Islam Talipov <italipov2022@gmail.com>
2026-07-20 02:40:03 +0300

    modal bug fixes

--- rzk/src/Rzk/TypeCheck/Unify.hs
+++ rzk/src/Rzk/TypeCheck/Unify.hs
@@ -485,8 +485,7 @@ unifyInCurrentContext mterm expected actual = performing action $ do
             _ -> err
         ModExtractT _ty app inn te ->
           case actual' of
-            ModExtractT _ty' app' inn' te' -> do
-              when (app' /= app) err
+            ModExtractT _ty' _app' inn' te' -> do
               when (inn' /= inn) err
               enterModality app $ unify Nothing te te'
             _ -> err
